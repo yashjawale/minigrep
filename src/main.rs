026df7d2077e728184619593156a7729886e8914
yashjawale <yash25.j@gmail.com>
2025-06-29 23:03:38 +0530

    feat: add search function

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,15 @@ fn main() {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	vec![]
+	let mut results = Vec::new();
+	
+	for line in contents.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
+
+	results
 }
 
 #[cfg(test)]
@@ -33,7 +41,7 @@ mod tests {
 		let query = "duct";
 		let contents = "\
 Rust:
-safe, fast, productive.
+safe, fast, productive
 Pick three.";
 
 		assert_eq!(vec!["safe, fast, productive"], search(query, contents));
