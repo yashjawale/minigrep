722763856ba64bdd3af0795680efbdea2a0b4498
yashjawale <yash25.j@gmail.com>
2025-06-29 21:11:41 +0530

    refactor: rename filepath to file_path

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ fn main() {
 	
 	let config = parse_config(&args);
 
-	let content = fs::read_to_string(&config.filepath).expect("Unable to read the file");
+	let content = fs::read_to_string(&config.file_path).expect("Unable to read the file");
 
 	println!("Searching for {}", config.query);
-	println!("In the file {}", config.filepath);
+	println!("In the file {}", config.file_path);
 	println!("With the content {}", content);
 }
 
 struct Config {
 	query: String,
-	filepath: String
+	file_path: String
 }
 
 fn parse_config(args: &[String]) -> Config {
 	let query = args[1].clone();
-	let filepath = args[2].clone();
+	let file_path = args[2].clone();
 
-	Config { query, filepath }
+	Config { query, file_path }
 }
