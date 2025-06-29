40cd412b9ef5f89623c93b24d4ce65452457d49e
yashjawale <yash25.j@gmail.com>
2025-06-29 21:14:17 +0530

    refactor: add constructor to Config struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 	
-	let config = parse_config(&args);
+	let config = Config::new(&args);
 
 	let content = fs::read_to_string(&config.file_path).expect("Unable to read the file");
 
@@ -18,9 +18,11 @@ struct Config {
 	file_path: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-	let query = args[1].clone();
-	let file_path = args[2].clone();
+impl Config {
+	fn new(args: &[String]) -> Config {
+		let query = args[1].clone();
+		let file_path = args[2].clone();
 
-	Config { query, file_path }
+		Config { query, file_path }
+	}
 }
