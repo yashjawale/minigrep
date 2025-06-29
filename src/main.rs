9e611b7746303bafa5a1e5dea956472864b34460
yashjawale <yash25.j@gmail.com>
2025-06-29 20:57:59 +0530

    feat: read file at given path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,9 @@ fn main() {
 	let query = &args[1];
 	let filepath = &args[2];
 
+	let content = fs::read_to_string(filepath).expect("Unable to read the file");
+
 	println!("Searching for {}", query);
 	println!("In the file {}", filepath);
+	println!("With the content {}", content);
 }
