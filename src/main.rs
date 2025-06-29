61f69d43d7e53c033dd9aef8e272691028bd6bf8
yashjawale <yash25.j@gmail.com>
2025-06-29 21:11:20 +0530

    feat: create Config struct to store query and filepath

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 	
-	let (query, filepath) = parse_config(&args);
+	let config = parse_config(&args);
 
-	let content = fs::read_to_string(filepath).expect("Unable to read the file");
+	let content = fs::read_to_string(&config.filepath).expect("Unable to read the file");
 
-	println!("Searching for {}", query);
-	println!("In the file {}", filepath);
+	println!("Searching for {}", config.query);
+	println!("In the file {}", config.filepath);
 	println!("With the content {}", content);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-	let query = &args[1];
-	let filepath = &args[2];
-	(query, filepath)
+struct Config {
+	query: String,
+	filepath: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+	let query = args[1].clone();
+	let filepath = args[2].clone();
+
+	Config { query, filepath }
 }
